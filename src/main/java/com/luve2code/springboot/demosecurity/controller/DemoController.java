347802a37c7abc43147bfc7cc8669e353650f872
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-10-08 20:50:19 +0300

    Restrict URLS Based on Roles

--- src/main/java/com/luve2code/springboot/demosecurity/controller/DemoController.java
+++ src/main/java/com/luve2code/springboot/demosecurity/controller/DemoController.java
@@ -13,4 +13,15 @@ public class DemoController {
         return "home";
     }
 
+
+    @GetMapping("/leaders")
+    public String showLeaders(){
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showAdmins(){
+        return "systems";
+    }
+
 }
